afc5d517c8ae35ce6b1f6e39613d81602b0dcb26
Cihat Şenel <70061200+cihatsnl34@users.noreply.github.com>
2022-05-20 23:10:27 +0300

    sql dosyası eklendi

--- db-file/flowersonline.sql
+++ db-file/flowersonline.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 14 May 2022, 15:10:35
+-- Üretim Zamanı: 20 May 2022, 22:09:39
 -- Sunucu sürümü: 10.4.24-MariaDB
--- PHP Sürümü: 7.4.29
+-- PHP Sürümü: 7.4.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -64,6 +64,38 @@ CREATE TABLE `failed_jobs` (
 -- --------------------------------------------------------
 
 --
+-- Tablo için tablo yapısı `flowers`
+--
+
+CREATE TABLE `flowers` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `flowerName` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` int(11) NOT NULL,
+  `anaResim` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `altResim` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `stokMiktar` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Tablo döküm verisi `flowers`
+--
+
+INSERT INTO `flowers` (`id`, `flowerName`, `price`, `anaResim`, `altResim`, `stokMiktar`, `created_at`, `updated_at`) VALUES
+(6, 'Gül', 200, '2022052018415.png', '[\"2022052014144.png\"]', 19, '2022-05-20 11:14:50', '2022-05-20 16:45:09'),
+(7, 'Menekşe', 100, '2022052018402.png', '[\"202205201840banner.jpg\"]', 0, '2022-05-20 15:40:43', '2022-05-20 15:53:24'),
+(8, 'Papatya', 150, '2022052018411.png', '[\"202205201841banner.jpg\"]', 49, '2022-05-20 15:41:15', '2022-05-20 15:59:42'),
+(9, 'Diğer Gül', 30, '2022052018421.png', '[\"202205201842banner.jpg\"]', 200, '2022-05-20 15:42:08', '2022-05-20 15:42:08'),
+(10, 'Akasya Çiçeği', 35, '2022052018445.png', '[\"202205201844banner.jpg\"]', 50, '2022-05-20 15:44:44', '2022-05-20 15:44:44'),
+(11, 'Ateş çiçeği', 30, '2022052018454.png', '[\"202205201845banner.jpg\"]', 50, '2022-05-20 15:45:06', '2022-05-20 15:45:06'),
+(12, 'Kaktüs', 300, '2022052018463.png', '[\"202205201846banner.jpg\"]', 35, '2022-05-20 15:46:22', '2022-05-20 15:46:22'),
+(13, 'Lale', 200, '2022052018461.png', '[\"202205201846banner.jpg\"]', 50, '2022-05-20 15:46:45', '2022-05-20 15:46:45'),
+(14, 'Orkide', 300, '2022052018472.png', '[\"202205201847banner.png\"]', 49, '2022-05-20 15:47:16', '2022-05-20 15:59:23');
+
+-- --------------------------------------------------------
+
+--
 -- Tablo için tablo yapısı `migrations`
 --
 
@@ -82,7 +114,11 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
 (4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(5, '2022_02_18_093949_create_admins_table', 1);
+(5, '2022_02_18_093949_create_admins_table', 1),
+(6, '2022_05_19_130710_create_flowers_table', 2),
+(7, '2022_05_19_133627_flower', 2),
+(8, '2022_05_20_145456_create_siparis_table', 3),
+(9, '2022_05_20_150042_siparis', 3);
 
 -- --------------------------------------------------------
 
@@ -117,6 +153,35 @@ CREATE TABLE `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Tablo için tablo yapısı `siparis`
+--
+
+CREATE TABLE `siparis` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `userId` int(11) NOT NULL,
+  `flowerId` int(11) NOT NULL,
+  `flowerAdet` int(11) NOT NULL,
+  `Adres` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `siparisDurum` tinyint(4) NOT NULL DEFAULT 0,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Tablo döküm verisi `siparis`
+--
+
+INSERT INTO `siparis` (`id`, `userId`, `flowerId`, `flowerAdet`, `Adres`, `siparisDurum`, `created_at`, `updated_at`) VALUES
+(3, 2, 6, 1, 'Pınar mahallesi 1505 sokak no 5 daire 2 istanbul/esenyurt', 1, '2022-05-20 12:41:39', '2022-05-20 14:50:44'),
+(4, 2, 6, 1, 'Pınar mahallesi 1505 sokak no 5 daire 2 istanbul/esenyurt', 1, '2022-05-20 12:50:40', '2022-05-20 14:50:44'),
+(8, 2, 7, 10, 'Pınar mahallesi 1505 sokak no 5 daire 2 istanbul/esenyurt', 1, '2022-05-20 15:53:24', '2022-05-20 15:55:33'),
+(9, 2, 14, 1, 'Pınar mahallesi 1505 sokak no 5 daire 2 istanbul/esenyurt', 1, '2022-05-20 15:59:23', '2022-05-20 16:00:58'),
+(10, 2, 8, 1, 'Pınar mahallesi 1505 sokak no 5 daire 2 istanbul/esenyurt', 1, '2022-05-20 15:59:42', '2022-05-20 16:00:58'),
+(11, 2, 6, 1, NULL, 0, '2022-05-20 16:45:09', '2022-05-20 16:45:09');
+
+-- --------------------------------------------------------
+
+--
 -- Tablo için tablo yapısı `users`
 --
 
@@ -130,6 +195,15 @@ CREATE TABLE `users` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Tablo döküm verisi `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(2, 'user', 'user@user.com', NULL, '$2y$10$ofMvPjwwVoNzdUSlmWCobOfDRjQMeafAaNWPPpBKNRc9i04y7dBm2', NULL, '2022-05-19 13:55:17', '2022-05-19 13:55:17'),
+(4, 'user', 'user2@user.com', NULL, '$2y$10$yg4vmoep491a2vvzHC1/B.hNmvg5Lbgq61RGQfWv7HIFLqLfaY2Cm', NULL, '2022-05-19 14:31:09', '2022-05-19 14:31:09'),
+(5, 'Cihat Şenel', 'cihat-cy@hotmail.com', NULL, '$2y$10$Rr9F.L3vz9a1auBCDR/LHups.mg4FqDLWzvpX6Eqqn1xP4n5awVMy', NULL, '2022-05-19 22:15:53', '2022-05-19 22:15:53');
 
 --
 -- Dökümü yapılmış tablolar için indeksler
@@ -148,6 +222,12 @@ ALTER TABLE `admins`
 ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Tablo için indeksler `flowers`
+--
+ALTER TABLE `flowers`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Tablo için indeksler `migrations`
@@ -170,6 +250,12 @@ ALTER TABLE `personal_access_tokens`
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
+-- Tablo için indeksler `siparis`
+--
+ALTER TABLE `siparis`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Tablo için indeksler `users`
 --
 ALTER TABLE `users`
@@ -184,7 +270,7 @@ ALTER TABLE `users`
 -- Tablo için AUTO_INCREMENT değeri `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `failed_jobs`
@@ -193,10 +279,16 @@ ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- Tablo için AUTO_INCREMENT değeri `flowers`
+--
+ALTER TABLE `flowers`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
 -- Tablo için AUTO_INCREMENT değeri `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `personal_access_tokens`
@@ -205,10 +297,16 @@ ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- Tablo için AUTO_INCREMENT değeri `siparis`
+--
+ALTER TABLE `siparis`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- Tablo için AUTO_INCREMENT değeri `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
